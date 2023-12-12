de6fd6a3698ea39c3487effe429c595ef2f92322
Deniz Sağlam <reincore@users.noreply.github.com>
2023-12-13 02:48:12 +0300

    Realtime database config added, cleaned gitignore,  added few npm scripts for ease of development

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, addDoc, getDocs, doc, deleteDoc } from "firebase/firestore";
 
-// Initialize Firebase
-// To-do: Replace with the Firebase project configuration
-firebase.initializeApp({
+
+// Firebase configuration
+const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID
-});
+};
 
-firebase.initializeApp(firebaseConfig);
-
-var db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 function initMap() {
     var map = L.map('map').setView([41.0082, 28.9784], 13); // Istanbul coordinates
@@ -24,18 +24,21 @@ function initMap() {
         attribution: '© OpenStreetMap contributors'
     }).addTo(map);
 
-    map.on('click', function(e) {
+    map.on('click', async function(e) {
         var customText = prompt("Enter text for the pin:", "Default Text");
         if (customText != null && customText !== "") {
             var newPin = { lat: e.latlng.lat, lng: e.latlng.lng, text: customText };
-            db.collection("pins").add(newPin).then((docRef) => {
+            try {
+                const docRef = await addDoc(collection(db, "pins"), newPin);
                 addMarker(newPin, map, docRef.id);
-            });
+            } catch (error) {
+                console.error("Error adding document: ", error);
+            }
         }
     });
 
     // Load existing pins
-    db.collection("pins").get().then((querySnapshot) => {
+    getDocs(collection(db, "pins")).then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             var data = doc.data();
             addMarker(data, map, doc.id);
@@ -46,11 +49,11 @@ function initMap() {
 function addMarker(pin, map, docId) {
     var marker = L.marker([pin.lat, pin.lng]).addTo(map)
         .bindPopup(pin.text)
-        .on('click', function() { // If user clicks on an existing pin
+        .on('click', function() {
             var confirmDeletion = confirm("Are you sure you want to delete this pin?");
             if (confirmDeletion) {
                 this.remove(); // Remove marker from map
-                db.collection("pins").doc(docId).delete(); // Remove pin data from Firebase
+                deleteDoc(doc(db, "pins", docId)); // Remove pin data from Firebase
             }
         });
 }
